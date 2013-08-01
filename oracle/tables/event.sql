8b6013162bfbdf04774896ea14f38b505b03701c
Robin Long <loveingenioustech@gmail.com>
2013-08-01 14:08:21 +0800

    update event table

--- oracle/tables/event.sql
+++ oracle/tables/event.sql
@@ -1,4 +1,6 @@
 CREATE TABLE event(id number NOT NULL,
                    title VARCHAR2(50) NOT NULL,
                    start_date DATE NOT NULL,
+                   update_user_id VARCHAR2(50),
+                   update_time DATE,
                    CONSTRAINT PK_EVENT PRIMARY KEY(id)) LOGGING;
