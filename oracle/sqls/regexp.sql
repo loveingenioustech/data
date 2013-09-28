441d2e0c0d5ea3c0be0048c4f63d3c964b4f1862
Robin Long <loveingenioustech@gmail.com>
2013-09-28 09:06:55 +0800

    update regexp.sql

--- oracle/sqls/regexp.sql
+++ oracle/sqls/regexp.sql
@@ -6,3 +6,19 @@ select regexp_substr('select streams_pool_size_for_estimate s,           streams
 select regexp_replace(regexp_substr('select streams_pool_size_for_estimate s,           streams_pool_size_factor * 100 f,           estd_spill_time + estd_unspill_time, 0  from study.v$streams_pool_advice where',
                      '\from\W+[a-zA-Z0-9$_.]+\'), '\from\W\')
   from dual;
+  
+--tokenize ONE single string  
+ DECLARE
+   CURSOR cur IS WITH qry AS(
+     SELECT 'Paris#London#Rome#Oslo#Amsterdam#New York' city_string
+       FROM dual)
+     SELECT regexp_substr(city_string, '[^#]+', 1, ROWNUM) city
+       FROM qry
+     CONNECT BY LEVEL <= LENGTH(regexp_replace(city_string, '[^#]+')) + 1;
+ 
+ BEGIN
+   FOR rec IN cur LOOP
+     dbms_output.put_line('City:' || rec.city);
+   END LOOP;
+ END;
+   
